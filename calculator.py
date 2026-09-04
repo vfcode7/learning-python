71fde3609e1a45dbe859feb964a67dcd3163f2c9
vinxz ogz <viniciusfaus33@gmail.com>
2026-09-04 19:13:06 -0300

    Corrigir calculadora: tratar divisão por zero e repetição de operações

--- calculator.py
+++ calculator.py
@@ -1,29 +1,34 @@
-print("Bem-vindo a calculadora")
-print("Escolha a operação desejada:")
-print("1. Adição")
-print("2. Subtração")
-print("3. Multiplicação")
-print("4. Divisão")
-
-operacao = input("Digite sua escolha (1/2/3/4): ")
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))   
-
-if operacao == '1':
-    print(num1 + num2)
-elif operacao == '2':
-    print(num1 - num2)
-elif operacao == '3':
-    print(num1 * num2)
-elif operacao == '4':
-    print(num1 / num2)
-else:
-    print("Opção inválida") 
-
-    print("Deseja realizar outra operação? (s/n)")
-    continuar = input().lower()
-    if continuar == 's':
-        exec(open("calculator.py").read())
+while True:
+    print("\nBem-vindo a calculadora")
+    print("Escolha a operação desejada:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+    
+    operacao = input("Digite sua escolha (1/2/3/4): ")
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))   
+    
+    if operacao == '1':
+        resultado = num1 + num2
+        print(f"Resultado: {num1} + {num2} = {resultado}")
+    elif operacao == '2':
+        resultado = num1 - num2
+        print(f"Resultado: {num1} - {num2} = {resultado}")
+    elif operacao == '3':
+        resultado = num1 * num2
+        print(f"Resultado: {num1} * {num2} = {resultado}")
+    elif operacao == '4':
+        if num2 == 0:
+            print("Erro: Não é possível dividir por zero!")
+        else:
+            resultado = num1 / num2
+            print(f"Resultado: {num1} / {num2} = {resultado}")
     else:
+        print("Opção inválida!")
+    
+    continuar = input("\nDeseja realizar outra operação? (s/n): ").lower()
+    if continuar != 's':
         print("Obrigado por usar a calculadora!")
-        
+        break
